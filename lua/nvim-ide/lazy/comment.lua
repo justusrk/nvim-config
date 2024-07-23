7743716ef1398227b78553899af0c4016ff67c72
Justus Raju <mail2justusrk@gmail.com>
2024-07-23 12:56:28 +0530

    Add tsx and jsx comment support plugin

--- lua/nvim-ide/lazy/comment.lua
+++ lua/nvim-ide/lazy/comment.lua
@@ -1,8 +1,23 @@
 -- For commenting out code
 return {
     'numToStr/Comment.nvim',
+    dependencies = {
+        {
+            'JoosepAlviste/nvim-ts-context-commentstring', -- for commenting tsx or jsx
+            config = function() 
+                require('ts_context_commentstring').setup {
+                    enable_autocmd = false,
+                  }
+            end
+        }
+    },
     opts = {
         -- add any options here
     },
+    config = function() 
+        require('Comment').setup {
+            pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(), -- for tsx or jsx
+        }
+    end,
     lazy = false,
 }
